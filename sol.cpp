dbb268c3800c78c506c90f6556dd7ccbef9af980
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-26 11:21:53 +0800

    Add files via upload

---
+++ sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		long long x, y;
+		cin >> x >> y;
+		// check if the difference of 'x' and 'y' is greater than 1
+		cout << (x - y > 1 ? "YES" : "NO") << '\n';
+	}
+	return 0;
+}
