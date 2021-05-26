5ecc5ce8e0831e475d9a150718e710b42b8ddc49
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-05-26 11:24:32 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -11,6 +11,10 @@ int main() {
 		long long x, y;
 		cin >> x >> y;
 		// check if the difference of 'x' and 'y' is greater than 1
+		// proof:
+		// x - y = z
+		// if z has a prime divisor p -> (z / p), then we can subtract p
+		// from x in (z / p) times
 		cout << (x - y > 1 ? "YES" : "NO") << '\n';
 	}
 	return 0;
